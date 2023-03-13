ef11e1c7279881a5b9e4465eb16f447a560cb5b3
Marcia Matsumoto <tiemi.matsumoto@gmail.com>
2023-03-13 23:04:47 +0000

    removing unusable tests and adding saucedemo testing

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -2,40 +2,31 @@ import { test, expect } from '@playwright/test'
 
 test.describe.parallel("Login/Logout", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("http://zero.webappsecurity.com/index.html")
+    await page.goto("https://www.saucedemo.com/")
   })
 
-  test("Failed to log in with wrong username and password", async ({page}) => {
-    await page.click("#signin_button")
-    await page.type('#user_login', 'fail user')
-    await page.type('#user_password', 'fail password')
-    await page.click("text=Sign in")
-    const errorMessage = await page.locator('.alert-error')
-    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  test('Fail to log in with wrong username and password', async ({ page }) => {
+    await page.type('#user-name', 'fail user')
+    await page.type('#password', 'fail password')
+    await page.click('#login-button')
+    const errorMessage = await page.locator('.error-message-container')
+    await expect(errorMessage).toContainText('Epic sadface: Username and password do not match any user in this service')
   })
 
-  test("Failed to log in without adding input", async ({page}) => {
-    await page.click("#signin_button")
-    await page.click("text=Sign in")
-    const errorMessage = await page.locator('.alert-error')
-    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  test('Fail to log in with locked out user', async ({ page }) => {
+    await page.type('#user-name', 'locked_out_user')
+    await page.type('#password', 'secret_sauce')
+    await page.click('#login-button')
+    const errorMessage = await page.locator('.error-message-container')
+    await expect(errorMessage).toContainText('Epic sadface: Sorry, this user has been locked out.')
   })
 
-  test("Failed to log in with correct username and wrong password", async ({page}) => {
-    await page.click("#signin_button")
-    await page.type('#user_login', 'username')
-    await page.type('#user_password', 'fail password')
-    await page.click("text=Sign in")
-    const errorMessage = await page.locator('.alert-error')
-    await expect(errorMessage).toContainText('Login and/or password are wrong.')
-  })
-
-  test("Login to Zero Bank", async ({ page }) => {
-    await page.click("#signin_button")
-    await page.type('#user_login', 'username')
-    await page.type('#user_password', 'fail password')
-    await page.click("text=Sign in")
-    const accountSumaryTab = await page.locator('#account_sumary_tab')
-    await expect(accountSumaryTab).toBeVisible()
+  test("Login to Sauce Demo Store", async ({ page }) => {
+    await page.type('#user-name', 'standard_user')
+    await page.type('#password', 'secret_sauce')
+    await page.click('#login-button')
+    const backpackProductTitle = await page.locator('#item_4_title_link')
+    expect(backpackProductTitle).toHaveText('Sauce Labs Backpack')
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
   })
 })
